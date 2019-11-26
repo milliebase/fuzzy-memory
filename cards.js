12100e1082d3b8a93af681ca2c18cc4e3c5513fd
milliebase <betsyalva@milliebase.se>
2019-11-26 10:41:16 +0100

    create cards.js

---
+++ cards.js
@@ -0,0 +1,12 @@
+const cards = [
+    {name: 'Mulan', image: 'images/mulan.jpg'},
+    {name: 'Chi Fu', image: 'images/chi_fu.jpeg'},
+    {name: 'Yao', image: 'images/yao.jpg'},
+    {name: 'Cricket', image: 'images/cricket.jpg'},
+    {name: 'Mushu', image: 'images/mushu.jpg'},
+    {name: 'Ling', image: 'images/ling.jpg'},
+    {name: 'Chien Po', image: 'images/chien_po.jpeg'},
+    {name: 'Matchmaker', image: 'images/matchmaker.jpeg'},
+    {name: 'Grandma', image: 'images/grandma.jpg'},
+    {name: 'Chan Yu', image: 'images/chan_yu.jpg'},
+]
