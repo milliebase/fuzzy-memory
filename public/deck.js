eb5a2ef1037e3132de7f77dc581ccc816ebd99a4
milliebase <betsyalva@milliebase.se>
2019-12-06 14:06:09 +0100

    update deck

--- public/deck.js
+++ public/deck.js
@@ -1,12 +1,12 @@
 const deck = [
-	{ name: "Mulan", image: "images/mulan.jpg" },
-	{ name: "Chi Fu", image: "images/chi_fu.jpeg" },
-	{ name: "Yao", image: "images/yao.jpg" },
-	{ name: "Cricket", image: "images/cricket.jpg" },
-	{ name: "Mushu", image: "images/mushu.jpg" },
-	{ name: "Ling", image: "images/ling.jpg" },
-	{ name: "Chien Po", image: "images/chien_po.jpeg" },
-	{ name: "Matchmaker", image: "images/matchmaker.jpeg" },
-	{ name: "Grandma", image: "images/grandma.jpg" },
-	{ name: "Chan Yu", image: "images/chan_yu.jpg" }
+  { name: "Mulan", image: "mulan.jpg" },
+  { name: "Chi Fu", image: "chi_fu.jpeg" },
+  { name: "Yao", image: "yao.jpg" },
+  { name: "Cricket", image: "cricket.jpg" },
+  { name: "Mushu", image: "mushu.jpg" },
+  { name: "Ling", image: "ling.jpg" },
+  { name: "Chien Po", image: "chien_po.jpeg" },
+  { name: "Matchmaker", image: "matchmaker.jpeg" },
+  { name: "Grandma", image: "grandma.jpg" },
+  { name: "Chan Yu", image: "chan_yu.jpg" }
 ];
