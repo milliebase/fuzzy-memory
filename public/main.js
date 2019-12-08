bb3e9c3ff9edfcaa224b4ca7b6ff0b4a809e6b33
milliebase <betsyalva@milliebase.se>
2019-12-08 22:05:18 +0100

    added multiplayer-function

--- public/main.js
+++ public/main.js
@@ -1,20 +1,78 @@
-////////////////////GAMEPLAY///////////////////////
 const gameBoard = document.querySelector(".game__board");
+
+// Overlay variables
 const overlay = document.querySelector(".overlay");
 const overlayTextH2 = document.querySelector(".overlay__text h2");
 const overlayTextP = document.querySelector(".overlay__text p");
 const replay = document.querySelector(".replay");
+
+//Array for choosed cards
 let chosenCards = [];
 
+//Deck and level variables
 let level;
 let gameDeck;
-let player;
+let numOfPlayers;
 
+// Stats variables
+const statsFirstPlayer = document.querySelector(".stats__players--one");
+const pointsFirstPlayer = document.querySelector(
+  ".stats__players--one h4:nth-of-type(2)"
+);
+
+const statsSecondPlayer = document.querySelector(".stats__players--two");
+const pointsSecondPlayer = document.querySelector(
+  ".stats__players--two h4:last-of-type"
+);
+
+//Keeping track of gameplay variables
+let currentTurn = "first player";
+
+let firstPlayerPoints = 0;
+let secondPlayerPoints = 0;
+
+////////////////////GAMEPLAY///////////////////////
+/**
+ * Function whichs toggles the game overlay
+ */
 const toggleOverlay = function() {
   overlay.classList.toggle("overlay--hidden");
   setTimeout(() => {
     overlay.classList.toggle("overlay--placement");
   }, 500);
+};
+
+/**
+ * Functions whichs controls which player's turn it is.
+ *
+ * @param {boolean} wasItAMatch
+ */
+const handleTurns = function(wasItAMatch) {
+  if (numOfPlayers === "1" && currentTurn === "first player") {
+    setTimeout(() => {
+      gameBoard.classList.remove("game__board--one");
+      gameBoard.classList.add("game__board--two");
+    }, 1000);
+
+    currentTurn = "second player";
+
+    if (wasItAMatch) {
+      firstPlayerPoints++;
+      pointsFirstPlayer.textContent = firstPlayerPoints;
+    }
+  } else if (currentTurn === "second player") {
+    setTimeout(() => {
+      gameBoard.classList.remove("game__board--two");
+      gameBoard.classList.add("game__board--one");
+    }, 1000);
+
+    currentTurn = "first player";
+
+    if (wasItAMatch) {
+      secondPlayerPoints++;
+      pointsSecondPlayer.textContent = secondPlayerPoints;
+    }
+  }
 };
 
 /**
@@ -37,6 +95,31 @@ const isMatch = function(chosenCards) {
   }
 };
 
+const getWinner = function(numOfPlayers) {
+  if (numOfPlayers === "0") {
+    overlayTextH2.textContent = "You won!";
+  }
+
+  if (numOfPlayers === "1" && firstPlayerPoints === secondPlayerPoints) {
+    overlayTextH2.textContent = "It's a tie!";
+  }
+
+  if (numOfPlayers === "1" && firstPlayerPoints > secondPlayerPoints) {
+    overlayTextH2.textContent = "Player 1 won!";
+  }
+
+  if (numOfPlayers === "1" && firstPlayerPoints < secondPlayerPoints) {
+    overlayTextH2.textContent = "Player 2 won!";
+  }
+
+  overlayTextP.textContent = "Play again or choose another level";
+
+  replay.classList.remove("replay--hidden");
+  setTimeout(() => {
+    toggleOverlay();
+  }, 500);
+};
+
 /**
  * Function with eventlisteners for playing the game
  */
@@ -47,32 +130,42 @@ const playGame = function() {
   //Eventlistener for flipping card
   cards.forEach(card => {
     card.addEventListener("click", event => {
-      event.currentTarget.classList.add("card--flip");
+      //Prevent from being able to click more than once
+      event.currentTarget.classList.add("card--locked");
 
+      if (numOfPlayers === "0") {
+        firstPlayerPoints++;
+        pointsFirstPlayer.textContent = firstPlayerPoints;
+      }
+
+      event.currentTarget.classList.add("card--flip");
       chosenCards.push(event.currentTarget);
 
       if (chosenCards.length === 2) {
         gameBoard.classList.add("game__board--locked");
 
-        let ifMatch = isMatch(chosenCards);
+        let wasItAMatch = isMatch(chosenCards);
 
-        if (ifMatch) {
+        if (numOfPlayers === "1") {
+          handleTurns(wasItAMatch);
+        }
+
+        if (wasItAMatch) {
           availableMatches--;
         }
 
         if (availableMatches === 0) {
-          overlayTextH2.textContent = "You won!";
-          overlayTextP.textContent = "Play again or choose another level";
-          replay.classList.remove("replay--hidden");
-          setTimeout(() => {
-            toggleOverlay();
-          }, 500);
+          getWinner(numOfPlayers);
         }
 
         chosenCards = [];
 
         setTimeout(() => {
           gameBoard.classList.remove("game__board--locked");
+
+          cards.forEach(card => {
+            card.classList.remove("card--locked");
+          });
         }, 950);
       }
     });
@@ -143,8 +236,14 @@ const generateLevel = function(cardLevelClass, gameDeck) {
   });
 };
 
-const checkChosenLevel = function(level) {
+const initiateChosenLevel = function(level) {
   gameBoard.innerHTML = "";
+
+  firstPlayerPoints = 0;
+  pointsFirstPlayer.textContent = firstPlayerPoints;
+
+  secondPlayerPoints = 0;
+  pointsSecondPlayer.textContent = secondPlayerPoints;
 
   shuffle(deck);
 
@@ -179,6 +278,8 @@ const playerButtons = document.querySelectorAll(".player__buttons");
 
 playerButtons.forEach(playerButton => {
   playerButton.addEventListener("click", event => {
+    numOfPlayers = event.currentTarget.dataset.players;
+
     event.currentTarget.classList.add("player__buttons--active");
 
     playerButtons.forEach(playerButton => {
@@ -186,28 +287,65 @@ playerButtons.forEach(playerButton => {
         playerButton.classList.remove("player__buttons--active");
       }
 
-      player = event.currentTarget.dataset.player;
+      levelButtons.forEach(levelButton => {
+        if (levelButton.classList.contains("level__buttons--active")) {
+          initiateChosenLevel(level);
+
+          if (
+            levelButton.classList.contains("level__buttons--active") &&
+            numOfPlayers === "1"
+          ) {
+            setTimeout(() => {
+              gameBoard.classList.add("game__board--one");
+            }, 100);
+          }
+        }
+      });
     });
-  });
-});
 
-//Eventlistener for each game level
-levelButtons.forEach(levelButton => {
-  levelButton.addEventListener("click", event => {
-    event.currentTarget.classList.add("level__buttons--active");
+    if (numOfPlayers === "0") {
+      statsFirstPlayer.classList.remove("stats__players--hidden");
+      statsSecondPlayer.classList.add("stats__players--hidden");
+      gameBoard.classList.remove("game__board--one");
+      gameBoard.classList.remove("game__board--two");
 
+      statsFirstPlayer.children[2].textContent = "clicks:";
+    }
+
+    if (numOfPlayers === "1") {
+      statsFirstPlayer.classList.remove("stats__players--hidden");
+      statsSecondPlayer.classList.remove("stats__players--hidden");
+
+      statsFirstPlayer.children[2].textContent = "points:";
+    }
+
+    //Eventlistener for each game level
     levelButtons.forEach(levelButton => {
-      if (levelButton !== event.currentTarget) {
-        levelButton.classList.remove("level__buttons--active");
-      }
-    });
+      levelButton.classList.add("level__buttons--clickable");
 
-    level = event.currentTarget.textContent;
-    checkChosenLevel(level);
+      levelButton.addEventListener("click", event => {
+        event.currentTarget.classList.add("level__buttons--active");
+
+        levelButtons.forEach(levelButton => {
+          if (levelButton !== event.currentTarget) {
+            levelButton.classList.remove("level__buttons--active");
+          }
+        });
+
+        if (numOfPlayers === "1") {
+          setTimeout(() => {
+            gameBoard.classList.add("game__board--one");
+          }, 400);
+        }
+
+        level = event.currentTarget.textContent;
+        initiateChosenLevel(level);
+      });
+    });
   });
 });
 
 //Eventlistener for replay button
 replay.addEventListener("click", () => {
-  checkChosenLevel(level);
+  initiateChosenLevel(level);
 });
